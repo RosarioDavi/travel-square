ddbfc1f145a8dd0b0022543132b8c4fc4138be40
Muhammad <mrahman9801@gmail.com>
2022-11-16 17:17:41 -0500

    data: modify venues, categories, states fields

--- relational-data/traveltwo.sql
+++ relational-data/traveltwo.sql
@@ -4,22 +4,22 @@ DROP TABLE IF EXISTS categories;
 
 CREATE TABLE states (
     id INTEGER NOT NULL UNIQUE,
-    name TEXT NOT NULL UNIQUE
+    state_name VARCHAR(2) NOT NULL UNIQUE
 );
 
 CREATE TABLE categories (
     id INTEGER NOT NULL UNIQUE
-    name TEXT NOT NULL UNIQUE
+    category_name VARCHAR(25) NOT NULL UNIQUE
 );
 
 CREATE TABLE venues (
     id INTEGER NOT NULL UNIQUE,
-    name TEXT NOT NULL,
+    venue_name VARCHAR(100) NOT NULL,
     street TEXT NOT NULL,
     city TEXT NOT NULL,
     state_id INTEGER REFERENCES states("id") ON DELETE CASCADE,
     category_id INTEGER REFERENCES categories("id") ON DELETE CASCADE,
-    description TEXT NOT NULL,
+    description_text VARCHAR(2000) NOT NULL,
     added_by INTEGER REFERENCES users("id") ON DELETE SET NULL,
     approved BOOLEAN DEFAULT false
 );
