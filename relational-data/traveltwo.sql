db407794e64787d02d3dfb51ea20328b631ab5c1
Muhammad <mrahman9801@gmail.com>
2022-11-16 18:18:39 -0500

    added followings and reviews_loved tables, modified reviews table to conform to rdbms

--- relational-data/traveltwo.sql
+++ relational-data/traveltwo.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS venues;
 DROP TABLE IF EXISTS states;
 DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS reviews_loved;
+DROP TABLE IF EXISTS followings;
 
 CREATE TABLE states (
     id INTEGER NOT NULL UNIQUE,
@@ -8,7 +11,7 @@ CREATE TABLE states (
 );
 
 CREATE TABLE categories (
-    id INTEGER NOT NULL UNIQUE
+    id INTEGER NOT NULL UNIQUE,
     category_name VARCHAR(25) NOT NULL UNIQUE
 );
 
@@ -17,19 +20,30 @@ CREATE TABLE venues (
     venue_name VARCHAR(100) NOT NULL,
     street TEXT NOT NULL,
     city TEXT NOT NULL,
-    state_id INTEGER REFERENCES states("id") ON DELETE CASCADE,
-    category_id INTEGER REFERENCES categories("id") ON DELETE CASCADE,
+    state_id INTEGER REFERENCES states('id') ON DELETE CASCADE,
+    category_id INTEGER REFERENCES categories('id') ON DELETE CASCADE,
     description_text VARCHAR(2000) NOT NULL,
-    added_by INTEGER REFERENCES users("id") ON DELETE SET NULL,
+    added_by INTEGER REFERENCES users('id') ON DELETE SET NULL,
     approved BOOLEAN DEFAULT false
 );
 
-CREATE TABLE review(
+CREATE TABLE reviews (
     id INTEGER NOT NULL UNIQUE,
     venue_id INTEGER REFERENCES venues('id') ON DELETE CASCADE,
     review_description TEXT NOT NULL,
     rating INTEGER,
     pictures TEXT NOT NULL,
-    added_by INTEGER REFERENCES users('id') ON DELETE CASCADE,
-    loved list INTEGER REFERENCES users('id') ON DELETE CASCADE
+    added_by INTEGER REFERENCES users('id') ON DELETE CASCADE
+)
+
+CREATE TABLE reviews_loved (
+    id INTEGER NOT NULL UNIQUE,
+    review_id INTEGER REFERENCES reviews('id') ON DELETE CASCADE,
+    loved_by INTEGER REFERENCES users('id') ON DELETE CASCADE
+)
+
+CREATE TABLE followings (
+    id INTEGER NOT NULL UNIQUE,
+    user_following INTEGER REFERENCES users('id') ON DELETE CASCADE,
+    user_followed INTEGER REFERENCES users('id') ON DELETE CASCADE
 )
