229f2847c73d0b08b39613b4ea51e096851e7830
Lena Zhang <zhanglena11@gmail.com>
2022-11-17 15:19:16 -0500

    data:create account and request queries and reviews

---
+++ fastapi-traveltwo/queries/request.py
@@ -0,0 +1,12 @@
+from models import RequestIn, Request, RequestOut
+from typing import List
+
+class RequestQueries(Queries):
+    DB_NAME =
+    COLLECTION =
+
+    def create(self, request: RequestIn) -> RequestOut:
+        props =
+
+    def get_all(self) -> List[RequestOut]:
+        result = self.
