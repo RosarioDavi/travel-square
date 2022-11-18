c97dbaf1b0109ddc682d3725baa76d23462ee14e
Muhammad <mrahman9801@gmail.com>
2022-11-18 12:04:41 -0500

    data: create migrations

--- fastapi-traveltwo/migrations/001_create_accounts_table.py
+++ fastapi-traveltwo/migrations/001_create_accounts_table.py
@@ -1,7 +1,18 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY PRIMARY KEY,
-    username VARCHAR(25) NOT NULL UNIQUE,
-    hashed_password VARCHAR(200) NOT NULL,
-    full_name VARCHAR(250)
-    avatar IMAGE,
-);
+steps = [
+    [
+        # Create table
+        """
+        CREATE TABLE accounts (
+            id SERIAL PRIMARY KEY NOT NULL,
+            username VARCHAR(25) NOT NULL UNIQUE,
+            hashed_password VARCHAR(200) NOT NULL,
+            full_name VARCHAR(250)
+            avatar TEXT
+        );
+        """,
+        # Drop table
+        """
+        DROP TABLE accounts
+        """
+    ]
+]
---
+++ fastapi-traveltwo/migrations/003_create_venues_table.py
@@ -0,0 +1,22 @@
+steps = [
+    [
+        # Create table
+        """
+        CREATE TABLE venues (
+            id SERIAL UNIQUE PRIMARY KEY,
+            venue_name VARCHAR(200) NOT NULL,
+            num_and_street TEXT NOT NULL UNIQUE,
+            city TEXT NOT NULL,
+            state_id INTEGER REFERENCES states('id') ON DELETE CASCADE,
+            category_id INTEGER REFERENCES categories('id') ON DELETE SET NULL,
+            description_text VARCHAR(2000) NOT NULL,
+            added_by INTEGER REFERENCES accounts('id') ON DELETE SET NULL,
+            approved BOOLEAN DEFAULT false
+        );
+        """,
+        # Drop table
+        """
+        DROP TABLE venues
+        """
+    ]
+]
