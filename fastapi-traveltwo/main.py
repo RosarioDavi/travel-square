2f6933c68f5efb57fc7ba6d57e2f02d98368846c
Rosario <rosariodavi9@gmail.com>
2022-11-21 15:31:35 -0500

    data: resolve merge conflicts

--- fastapi-traveltwo/main.py
+++ fastapi-traveltwo/main.py
@@ -5,7 +5,6 @@ from authenticator import authenticator
 
 app = FastAPI()
 
-app = FastAPI()
 app.include_router(venues.router)
 app.include_router(authenticator.router)
 app.include_router(accounts.router)
