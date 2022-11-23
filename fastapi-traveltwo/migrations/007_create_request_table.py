24b8dbc0acb24b7d0be9fb16eab7ef7e743f8ab6
Lena Zhang <zhanglena11@gmail.com>
2022-11-23 15:22:10 -0500

    data: working on innerjoins

--- fastapi-traveltwo/migrations/007_create_request_table.py
+++ fastapi-traveltwo/migrations/007_create_request_table.py
@@ -6,8 +6,7 @@ steps = [
             id SERIAL UNIQUE PRIMARY KEY,
             requester INTEGER REFERENCES accounts("id") ON DELETE CASCADE,
             txt TEXT NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
-        );
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
         """,
         # Drop table
         """
