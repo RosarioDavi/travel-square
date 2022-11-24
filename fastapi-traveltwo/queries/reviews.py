a089ff7c7ec82cc004201e91fcc5afaf7a26b86d
Sarah Ahn <sarahahn@gmail.com>
2022-11-23 23:10:58 -0600

    data: add get all reviews method in queries and routers

--- fastapi-traveltwo/queries/reviews.py
+++ fastapi-traveltwo/queries/reviews.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import date
 from queries.pool import pool
+from typing import List
 
 
 class ReviewIn(BaseModel):
@@ -22,13 +23,33 @@ class ReviewOut(BaseModel):
 
 
 class ReviewQueries:
-    def get_all_reviews_for_venue(self) -> list[ReviewOut]:
+    def get_all_reviews(self) -> List[ReviewOut]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(
                         """
                         SELECT id, review_description, rating, picture, added_by, created_at
+                        FROM reviews
+                        ORDER BY created_at;
+                        """
+                    )
+
+                    results = []
+                    for row in cur.fetchone():
+                        record = {}
+                        for i, column in enumerate(cur.description):
+                            record[column.name] = row[i]
+                        results.append(record)
+                    return results
+
+    def get_all_reviews_for_venue(self) -> List[ReviewOut]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        SELECT id, venue_id, review_description, rating, picture, added_by, created_at
                         FROM reviews
                         ORDER BY created_at;
                         """
@@ -100,105 +121,18 @@ class ReviewQueries:
             print(e)
             return {"message": "Could not create a new review"}
 
-    def delete_review(self, id: int) -> bool:
-        try:
-            with pool.connection() as conn:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        DELETE FROM reviews
-                        WHERE id = %s
-                        """,
-                        [id]
-                    )
-                    return True
-        except Exception as e:
-            print(e)
-            return False
-
-
-
-
-    # def record_to_reviews_out(self, review):
-    #     return ReviewOut(
-    #         id=record[0],
-    #         review_description=record[1],
-    #         rating=record[2],
-    #         pictures=record[3],
-    #         created_by=record[4],
-    #         created_date=record[5]
-    #     )
-
-    # def reviews_in_to_out(self, id: int: ReviewIn):
-    #     old_data = review.dict()
-    #     return ReviewOut(id=id, **old_data)
-
-
-# class ReviewRepository:
-#     def get_all(self):
-#         try:
-#             # connect the database
-#             with pool.connection() as conn:
-#                 # get a cursor (something to run SQL with)
-#                 with conn.cursor() as db:
-#                     # run our SELECT statement
-#                     result = db.execute(
-#                         """
-#                         SELECT id, name
-#                         FROM reviews
-#                         ORDER BY
-#                         """
-#                     )
-#         except Exception:
-#             return {"message": "Could not get all reviews"}
-
-
-
-# class ReviewQueries(Queries):
-#     DB_NAME = "travelsquared"
-#     COLLECTION = "reviews"
-
-#     def get(self, review: str):
-#         return self.collection.find_one({"review": review})
-
-    # def create(self, review: str, account: Account) -> Optional[Account]:
-    #     result = self.collection.insert_one(
-    #         {
-    #             "review": review,
-    #             "account_id": ObjectId(account.id),
-    #         }
-    #     )
-    #     if result and result.inserted_id:
-    #         return SessionOut(review=review, account_id=account.id)
-    #     return None
-
-    # def delete(self, review: str):
-    #     self.collection.delete_many({"review": review})
-
-    # def create(self, review: ReviewIn) -> ReviewOut:
-    #     props = review.dict()
-    #     self.collection.insert_one(props)
-    #     props["id"] = str(props["_id"])
-    #     return ReviewOut(loans=[], **props)
-
-    # def get_all(self) -> List[ReviewOut]:
-    #     result = self.collection.aggregate(
-    #         [
-    #             {
-    #                 "$lookup": {
-    #                     "from": "loans",
-    #                     "localField": "_id",
-    #                     "foreignField": "book_id",
-    #                     "as": "loans",
-    #                 }
-    #             },
-    #             {"$sort": {"title": 1}},
-    #         ]
-    #     )
-    #     reviewPropsList = list(result)
-    #     for reviewProps in reviewPropsList:
-    #         reviewProps["id"] = str(reviewProps["_id"])
-    #         reviewProps["loans"] = [
-    #             str(props["account_id"]) for props in reviewProps["loans"]
-    #         ]
-    #     return [ReviewOut(**review) for review in reviewPropsList]
+    # def delete_review(self, id: int) -> bool:
+    #     try:
+    #         with pool.connection() as conn:
+    #             with conn.cursor() as cur:
+    #                 cur.execute(
+    #                     """
+    #                     DELETE FROM reviews
+    #                     WHERE id = %s
+    #                     """,
+    #                     [id]
+    #                 )
+    #                 return True
+    #     except Exception as e:
+    #         print(e)
+    #         return False
