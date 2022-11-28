4d99f6070eaa6eaecbf9f7a2e2f207864c84b502
Muhammad <mrahman9801@gmail.com>
2022-11-28 17:09:17 -0500

    accounts: add exc to exception on router

--- fastapi-traveltwo/routers/accounts.py
+++ fastapi-traveltwo/routers/accounts.py
@@ -87,12 +87,17 @@ async def create_account(
     avatar = ''
     is_admin = False
     try:
-        account = accounts.create_account(info, hashed_password, avatar, is_admin)
-    except DuplicateAccountError:
+        account = accounts.create_account(
+            info,
+            hashed_password,
+            avatar,
+            is_admin
+        )
+    except DuplicateAccountError as exc:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Cannot create an account with those credentials",
-        )
+        ) from exc
     form = AccountForm(
         username=info.username,
         password=info.password
