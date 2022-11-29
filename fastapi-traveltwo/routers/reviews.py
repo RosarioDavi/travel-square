25e38a89584dee9fda1136a2b320d5a825c7f830
Sarah Ahn <sarahahn@gmail.com>
2022-11-29 16:24:32 -0600

    data: review queries and routers

--- fastapi-traveltwo/routers/reviews.py
+++ fastapi-traveltwo/routers/reviews.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from queries.reviews import ReviewQueries
-from typing import List
-from queries.reviews import ReviewIn, ReviewOut
+from queries.reviews import ReviewQueries, ReviewIn, ReviewOut
 from datetime import date
 
 
 router = APIRouter()
 
 
-@router.get("/api/reviews/", response_model=ReviewOut)
+@router.get("/api/reviews/{state}/{city}", response_model=list[ReviewOut])
 def get_all_reviews(
+    state: str,
+    city: str,
     repo: ReviewQueries = Depends(),
 ):
-    return repo.get_all_reviews()
+    return repo.get_all_reviews(state, city)
 
 
 @router.post("/api/reviews/", response_model=ReviewOut)
@@ -24,17 +24,20 @@ def create_review(
     created_at = date.today()
     return repo.create_review(review, created_at)
 
-@router.get("/api/venues/{venue_id}/reviews/", response_model=List[ReviewOut])
+@router.get("/api/venues/{venue_id}/reviews/", response_model=list[ReviewOut])
 def get_all_reviews_for_venue(
+    venue_id: int,
     repo: ReviewQueries = Depends(),
 ):
-    return repo.get_all_reviews_for_venue()
+    return repo.get_all_reviews_for_venue(venue_id)
 
 @router.get("/api/venues/{venue_id}/{review_id}/", response_model=ReviewOut)
 def get_one_review_for_venue(
+    venue_id: int,
+    review_id: int,
     repo: ReviewQueries = Depends(),
 ):
-    return repo.get_one_review_for_venue()
+    return repo.get_one_review_for_venue(venue_id, review_id)
 
 # @router.delete("/api/venues/{venue_id}/{review_id}", response_model=ReviewOut)
 # def delete_review(
