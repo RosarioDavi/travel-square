8056354df1cdc4cd0df429f483b7edce57b33962
Lena Zhang <zhanglena11@gmail.com>
2022-12-01 16:41:54 -0500

    chore: fix Homepage.js

--- ghi/src/Homepage.js
+++ ghi/src/Homepage.js
@@ -1,21 +1,25 @@
 import React from "react";
 
-function Homepage() {
-  <div className="cover">
-    <h1>discover what's out there.</h1>
-    <form className="flex-form">
-      <label for="from">
-        <i className="ion-location"></i>
-      </label>
-      <input type="search" placeholder="where do you want to go?" />
-      <input type="submit" value="search" />
-    </form>
-    <div id="madeby">
-      <span>
-        Photo by <a href="https://unsplash.com/@benblenner" target="_blank"></a>
-      </span>
+function HomePage() {
+  return (
+    <div className="cover">
+      <h1>discover what's out there.</h1>
+      <form className="flex-form">
+        <label for="from">
+          <i className="ion-location"></i>
+        </label>
+        <input type="search" placeholder="where do you want to go?" />
+        <input type="submit" value="search" />
+      </form>
+      <div id="madeby">
+        <span>
+          Photo by{" "}
+          <a href="https://unsplash.com/@benblenner" target="_blank">
+            Ben Blennerhassett
+          </a>
+        </span>
+      </div>
     </div>
-  </div>;
+  );
 }
-
-export default Homepage;
+export default HomePage;
