f31812f7487b3c8b3dfecf1e45c8199071285258
Lena Zhang <zhanglena11@gmail.com>
2022-12-09 16:27:57 -0500

    feat: show reviews modal

---
+++ ghi/src/Explore/ShowReviews.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export default function ShowReview(props) {
+  const [reviews, setReviews] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // const venue = props.venue.id;
+
+  const fetchData = async () => {
+    const data = props.venue.id;
+    const commentUrl = `http://localhost:8000/api/venues/${data}/reviews/`;
+    const response = await fetch(commentUrl);
+    const newData = await response.json();
+    setReviews(newData);
+  };
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Reviews!
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Reviews</Modal.Title>
+        </Modal.Header>
+        {reviews.map((review) => {
+          return (
+            <Modal.Body key={review.id}>
+              {review.username} {""}
+              {review.created_at} : {""}
+              {review.txt}
+            </Modal.Body>
+          );
+        })}
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
