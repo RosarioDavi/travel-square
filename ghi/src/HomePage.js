5fc66b8f6455541154bce97959c799d9cbdf535c
Sarah Ahn <sarahahn@gmail.com>
2022-12-12 16:58:45 -0600

    style: capitalize discover

--- ghi/src/HomePage.js
+++ ghi/src/HomePage.js
@@ -5,7 +5,7 @@ import "./Nav.css";
 export default function HomePage() {
   return (
     <div className="cover">
-      <h1>discover what's out there.</h1>
+      <h1>Discover what's out there.</h1>
       <form className="flex-form">
         <label htmlFor="from">
         </label>
@@ -14,14 +14,6 @@ export default function HomePage() {
         <input type="search" placeholder="State" />
         <input type="submit" value="search" />
       </form>
-      {/* <div id="madeby">
-        <span>
-          Photo by{" "}
-          <a href="https://unsplash.com/@benblenner">
-            Ben Blennerhassett
-          </a>
-        </span>
-      </div> */}
     </div>
   );
 }
